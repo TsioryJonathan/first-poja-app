32334ecc0fdd4a81b0410c2b065da5ba639e7e1e
Tsiory Jonathan <rakotoarisoajonathan0@gmail.com>
2026-05-26 09:18:04 +0300

    feat(test): add test ok and failed

--- src/test/java/com/example/demo/conf/AddTest.java
+++ src/test/java/com/example/demo/conf/AddTest.java
@@ -1,18 +1,31 @@
 package com.example.demo.conf;
-
-import com.example.demo.PojaApplication;
+import com.example.demo.service.AddService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class AddTest {
-
-
+    AddService addService;
+    @BeforeEach
+    void setup(){
+        addService = new AddService();
+    }
     @Test
     void addOk(){
-
-
+        int a = 10;
+        int b = 50;
+        assertEquals(60,addService.add(a,b));
     }
+    @Test
     void addFail(){
-
+        int a = 10;
+        int b = -5;
+        assertThrows(IllegalArgumentException.class, () -> addService.add(a,b));
+    }
+    @Test
+    void addNotEqual(){
+        int a = 10;
+        int b = 50;
+        assertNotEquals(70,addService.add(a,b));
     }
 }
