c1731dd2784fb868dc3e57011e7b6a11390f68ca
Tsiory Jonathan <rakotoarisoajonathan0@gmail.com>
2026-05-26 09:06:52 +0300

    feat: GET /add add two number

---
+++ src/test/java/com/example/demo/conf/AddTest.java
@@ -0,0 +1,18 @@
+package com.example.demo.conf;
+
+import com.example.demo.PojaApplication;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class AddTest {
+
+
+    @Test
+    void addOk(){
+
+
+    }
+    void addFail(){
+
+    }
+}
