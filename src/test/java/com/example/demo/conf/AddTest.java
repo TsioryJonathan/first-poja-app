bdbdb2b3911379eebdc5b56cd7ebe3c42557eb5f
Tsiory Jonathan <rakotoarisoajonathan0@gmail.com>
2026-05-26 09:20:35 +0300

    fix: add private to addService attribute in AddTest

--- src/test/java/com/example/demo/conf/AddTest.java
+++ src/test/java/com/example/demo/conf/AddTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class AddTest {
-    AddService addService;
+    private AddService addService;
     @BeforeEach
     void setup(){
         addService = new AddService();
