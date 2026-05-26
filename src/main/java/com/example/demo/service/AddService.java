3df53f7ab21bcd31465a6964df76d8ff113ef521
Tsiory Jonathan <rakotoarisoajonathan0@gmail.com>
2026-05-26 09:24:53 +0300

    chore: formate

--- src/main/java/com/example/demo/service/AddService.java
+++ src/main/java/com/example/demo/service/AddService.java
@@ -4,10 +4,10 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class AddService {
-    public int add(int a, int b) {
-        if(a < 0 || b < 0) {
-            throw new IllegalArgumentException("A or B cannot be negative");
-        }
-        return a + b;
+  public int add(int a, int b) {
+    if (a < 0 || b < 0) {
+      throw new IllegalArgumentException("A or B cannot be negative");
     }
+    return a + b;
+  }
 }
