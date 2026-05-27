2192d43da412e3d1546d4a5e492d0f9025102061
Tsiory Jonathan <rakotoarisoajonathan0@gmail.com>
2026-05-27 17:22:27 +0300

    feat: create library and book model

---
+++ src/main/java/com/poja/first/model/library/Book.java
@@ -0,0 +1,40 @@
+package com.poja.first.model.library;
+
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode(exclude = "library")
+@Table(name = "book")
+public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String author;
+
+    @Column(nullable = false, name = "release_year")
+    private Integer releaseYear;
+
+    @ManyToOne
+    @JoinColumn(name = "id_library")
+    private Library library;
+
+    public Book(String title, String author, Integer releaseYear, Library library) {
+        this.title = title;
+        this.author = author;
+        this.releaseYear = releaseYear;
+        this.library = library;
+    }
+
+}
---
+++ src/main/java/com/poja/first/model/library/Library.java
@@ -0,0 +1,49 @@
+package com.poja.first.model.library;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "library")
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString(exclude = "books")
+@Getter
+@Setter
+@EqualsAndHashCode(exclude = "books")
+public class Library {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String address;
+
+    @Column(name = "phone_number")
+    private String phoneNumber;
+
+    public Library(String name, String address, String phoneNumber) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    @OneToMany(mappedBy = "library", cascade = CascadeType.ALL)
+    private List<Book> books;
+
+    public void addBook(Book book) {
+        books.add(book);
+        book.setLibrary(this);
+    }
+
+    public void removeBook(Book book) {
+        books.remove(book);
+        book.setLibrary(null);
+    }
+}
